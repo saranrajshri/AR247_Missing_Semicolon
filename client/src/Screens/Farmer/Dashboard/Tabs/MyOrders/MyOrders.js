20e10b136e473b871410b0a6daade26bb8a8fd1e
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-08-02 01:47:46 +0530

    creates myorders and adds completed and current orders screen with menu navigation

--- client/src/Screens/Farmer/Dashboard/Tabs/MyOrders/MyOrders.js
+++ client/src/Screens/Farmer/Dashboard/Tabs/MyOrders/MyOrders.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MyOrders.css";
+import { useHistory } from "react-router-dom";
 import CurrentOrders from "./components/CurrentOrders/CurrentOrders";
+import CompletedOrders from "./components/CompletedOrders/CompletedOrders";
+import { Menu } from "semantic-ui-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const MyOrders = () => {
+  const history = useHistory();
+  const [activeItem, setActiveItem] = useState("currentOrders");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
+
   return (
-    <div>
-      <CurrentOrders />
-    </div>
+    <>
+      <div id="farmer-header">
+        <div className="logo-bar">
+          <div className="logo-name">
+            <FontAwesomeIcon
+              icon={faArrowLeft}
+              className="icon"
+              onClick={() => history.goBack()}
+            />
+          </div>
+        </div>
+      </div>
+      <Menu tabular widths="2" style={{ marginTop: "-1px" }}>
+        <Menu.Item
+          name="currentOrders"
+          active={activeItem === "currentOrders"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="completedOrders"
+          active={activeItem === "completedOrders"}
+          onClick={handleItemClick}
+        />
+      </Menu>
+      {activeItem === "currentOrders" ? <CurrentOrders /> : <CompletedOrders />}
+    </>
   );
 };
 
