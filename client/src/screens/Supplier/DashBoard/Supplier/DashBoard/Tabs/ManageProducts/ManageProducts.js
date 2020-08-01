e3eaad7c788a9d3d21f663dbc220e62256d3a831
Gowtham C <gowthamdhss@gmail.com>
2020-08-01 16:36:02 +0530

    Adds SearchProducts fro Supplier

--- client/src/screens/Supplier/DashBoard/Supplier/DashBoard/Tabs/ManageProducts/ManageProducts.js
+++ client/src/screens/Supplier/DashBoard/Supplier/DashBoard/Tabs/ManageProducts/ManageProducts.js
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from "react";
 
-import "./ManageProducts.css";
 
 import { Search } from "semantic-ui-react";
 import ProductsTable from "./components/ProductsTable";
@@ -9,12 +8,29 @@ import { Context } from "../../../../../Context/Context";
 import { deleteProduct, updateProduct } from "../../../../../actions/actions";
 
 const ManageProducts = () => {
+  const [searchValue, setSearchValue] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [editModal, setEditModal] = useState(false);
   const [editProduct, setEditProduct] = useState(undefined);
   const { products, supplierData, setAlert, setFullScreenLoader } = useContext(
     Context
   );
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+    if (e.target.value !== "") {
+      filterProducts();
+    } else {
+      setFilteredProducts([]);
+    }
+  };
+  // Filter products by product names
+  const filterProducts = () => {
+    let filteredProducts = products.filter((product) =>
+      product.productName.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredProducts(filteredProducts);
+  };
 
   // Set product to edit and open edit modal.
   const handleEdit = (product) => {
@@ -45,7 +61,16 @@ const ManageProducts = () => {
   return (
     <div className="p-5">
       <p className="title text-dark">Manage Products</p>
-      {/*TODO Search bar */}
+      {/* Search bar */}
+      <div className="search-bar">
+        <Search
+          onSearchChange={handleSearchChange}
+          open={false}
+          placeholder="Search Here"
+          value={searchValue}
+          minCharacters="2"
+        />
+      </div>
       
       {/* Products table */}
       <div className="table-wrapper">
