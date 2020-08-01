9e7bfbf423939fdd80ea9643caeac7b1b35d96a2
Shrisaranraj <saran.raj@gogosoon.com>
2020-08-02 02:04:25 +0530

    Adds Notification Component

--- client/src/Screens/Supplier/DashBoard/Tabs/Notifications/Notifications.js
+++ client/src/Screens/Supplier/DashBoard/Tabs/Notifications/Notifications.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../../../../../Context/Context";
 
 const Notifications = () => {
+  const { notifications } = useContext(Context);
   return <p>hello</p>;
 };
 export default Notifications;
