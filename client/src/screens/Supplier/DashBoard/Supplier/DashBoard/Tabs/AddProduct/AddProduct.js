130a4fcd4f084d2053820faf3690128b5befae40
Gowtham C <gowthamdhss@gmail.com>
2020-08-01 14:56:00 +0530

    UI Structure Added

--- client/src/screens/Supplier/DashBoard/Supplier/DashBoard/Tabs/AddProduct/AddProduct.js
+++ client/src/screens/Supplier/DashBoard/Supplier/DashBoard/Tabs/AddProduct/AddProduct.js
@@ -7,7 +7,15 @@ import { Form, Input, TextArea, Button, Divider } from "semantic-ui-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle, faEraser } from "@fortawesome/free-solid-svg-icons";
 
+// Context
+import { Context } from "../../../../../../../Context/Context";
+
+// TODO refs
+
+
 const AddProduct = () => {
+  const { setFullScreenLoader, setAlert, supplierData } = useContext(Context);
+
 
 
   // Product state
@@ -44,13 +52,94 @@ const AddProduct = () => {
     });
   };
 
+  // TODO Image Upload
+
   
 
   return (
     <div className="p-5">
       <p className="title text-dark">Add a new product</p>
       {/* Row */}
-      
+      <div className="grid-container-2">
+        <div className="grid-item">
+          <div className="card">
+            <div className="card-content" style={{ padding: 30 }}>
+              <Form>
+                <Form.Group widths="equal">
+                  <Form.Field
+                    control={Input}
+                    label="Barcode"
+                    required
+                    name="barCode"
+                    onChange={handleChange}
+                    placeholder="Barcode"
+                    value={product.barCode}
+                    error={error}
+                  />
+                  <Form.Field
+                    control={Input}
+                    label="Product Name"
+                    required
+                    name="productName"
+                    onChange={handleChange}
+                    placeholder="Product name"
+                    value={product.productName}
+                  />
+                </Form.Group>
+                <Form.Group widths="equal">
+                  <Form.Field
+                    control={Input}
+                    label="Product Price"
+                    required
+                    step={0.1}
+                    type="number"
+                    name="productPrice"
+                    onChange={handleChange}
+                    placeholder="Price"
+                    value={product.productPrice}
+                  />
+                  <Form.Field
+                    control={Input}
+                    label="In Stock Quantity (kg)"
+                    required
+                    type="number"
+                    name="availableQuantity"
+                    onChange={handleChange}
+                    placeholder="In Stock Quantity"
+                    value={product.availableQuantity}
+                  />
+                </Form.Group>
+                <Form.Field
+                  id="form-textarea-control-opinion"
+                  control={TextArea}
+                  label="Product Description"
+                  placeholder="Description"
+                  onChange={handleChange}
+                  name="productDescription"
+                  value={product.productDescription}
+                />
+                <Form.Checkbox
+                  checked={!product.isHidden}
+                  name="isHidden"
+                  onChange={(e) => {
+                    handleCheckBox(!product.isHidden);
+                  }}
+                  label="Publish Now"
+                />
+              </Form>
+              <Divider />
+              <Button onClick={clear}>
+                <FontAwesomeIcon icon={faEraser} className="icon" />
+                Clear
+              </Button>
+              <Button primary >
+                <FontAwesomeIcon icon={faPlusCircle} className="icon" />
+                Add
+              </Button>
+            </div>
+          </div>
+        </div>
+        </div>      
     </div>
   );
 };
