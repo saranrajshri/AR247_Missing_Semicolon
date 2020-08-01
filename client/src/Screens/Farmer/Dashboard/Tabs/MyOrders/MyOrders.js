1fee45e83929cf2e64656dc125bf18959c03e4c5
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-08-02 01:30:08 +0530

    Adds current order to my order

---
+++ client/src/Screens/Farmer/Dashboard/Tabs/MyOrders/MyOrders.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MyOrders.css";
+import CurrentOrders from "./components/CurrentOrders/CurrentOrders";
+
+const MyOrders = () => {
+  return (
+    <div>
+      <CurrentOrders />
+    </div>
+  );
+};
+
+export default MyOrders;
