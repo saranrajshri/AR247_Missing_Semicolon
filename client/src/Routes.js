affd0624c2a9f14302ced202cf7c0d24b1382895
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-08-01 20:13:12 +0530

    adds authentication to routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,12 +1,63 @@
 import React, { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Landing from "./Screens/Landing/Landing";
+
+import Login from "./Screens/Supplier/Login/Login";
+import Signup from "./Screens/Supplier/Signup/Signup";
+import Home from "./Screens/Supplier/DashBoard/Home";
+
+import Signin from "./Screens/Farmer/Signin/Signin";
+import Register from "./Screens/Farmer/Register/Register";
+import FarmerHome from "./Screens/Farmer/Dashboard/FarmerHome";
+import {
+  ProductDescription,
+  Checkout,
+  CartScreen
+} from "./Screens/Farmer/Dashboard/Tabs";
+
+import Driver from "./Screens/Driver/Driver";
+import DriverLogin from "./Screens/Driver/Login/Login";
+
 import { FarmerContext } from "./Context/FarmerContext";
 import { DriverContext } from "./Context/DriverContext";
-
+import {
+  checkCustomerAuthentication,
+  checkDriverAuthentication
+} from "./actions/actions";
 // Route function containing all routes for the app.
 const Routes = () => {
   const { setFarmerData } = useContext(FarmerContext);
   const { setDriverData } = useContext(DriverContext);
+
+  useEffect(() => {
+    const checkCustomer = () => {
+      checkCustomerAuthentication()
+        .then((res) => {
+          // console.log(res.data);
+          setFarmerData(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+          setFarmerData({});
+          localStorage.removeItem("CUSTOMER_AUTH_TOKEN");
+        });
+    };
+    const checkDriver = () => {
+      checkDriverAuthentication()
+        .then((res) => {
+          setDriverData(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+          setDriverData({});
+          localStorage.removeItem("DRIVER_AUTH_TOKEN");
+        });
+    };
+
+    checkCustomer();
+    checkDriver();
+  }, []);
 
   return (
     <div>
@@ -21,7 +72,6 @@ const Routes = () => {
           <PrivateRoute path="/driver/home" exact component={Driver} />
           <Route path="/driver/login" exact component={DriverLogin} />
           {/* Farmer routes */}
-
           <Route path="/farmer/login" exact component={Signin} />
           <Route path="/farmer/register" exact component={Register} />
           <Route path="/farmer/home" exact component={FarmerHome} />
