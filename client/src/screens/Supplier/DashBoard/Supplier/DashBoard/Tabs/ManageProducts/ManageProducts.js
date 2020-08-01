81fe921ba3be370ee9ae1cde9a21e28fddcaf073
Gowtham C <gowthamdhss@gmail.com>
2020-08-01 15:55:18 +0530

    Initializes ManageProducts for Supplier

---
+++ client/src/screens/Supplier/DashBoard/Supplier/DashBoard/Tabs/ManageProducts/ManageProducts.js
@@ -0,0 +1,6 @@
+import React, { useState, useContext } from "react";
+
+import { Search } from "semantic-ui-react";
+
+import { Context } from "../../../../../Context/Context";
+import { deleteProduct, updateProduct } from "../../../../../actions/actions";
