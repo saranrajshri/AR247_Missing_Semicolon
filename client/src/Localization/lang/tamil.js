2176a82a6e70397a84d033c7fa54ad3cbe1dcd57
Gowtham C <gowthamdhss@gmail.com>
2020-08-02 15:00:55 +0530

    Tamil Language Support Added

---
+++ client/src/Localization/lang/tamil.js
@@ -0,0 +1,68 @@
+export default {
+    translation: {
+      greeting: "Hello",
+      // CartScreen
+      checkout: "சரிபார்",
+      removeFromCart: "வண்டியில் இருந்து அகற்று",
+      goShop: "சில கடைக்குச் செல்லுங்கள்.",
+      cantLoad: "இந்த நேரத்தில் வண்டியை ஏற்ற முடியாது.",
+      cannotRemove: "உருப்படியை அகற்ற முடியாது.",
+      itemRemoved: "வண்டியில் இருந்து உருப்படி அகற்றப்பட்டது.",
+      emptyCart: "வண்டி காலியாக உள்ளது.",
+      logout: "வெளியேறு",
+      // Checkout
+      orderPlaced: "ஆர்டர் வைக்கப்பட்டுள்ளது.",
+      cannotOrder: "ஒழுங்கை வைக்க முடியாது.",
+      summary: "தயாரிப்பு சுருக்கம்",
+      gTotal: "மொத்தம்",
+      placeOrder: "ஆர்டர்களை வைக்கவும்",
+      customer: "வாடிக்கையாளர் விவரங்கள்",
+      contact: "தொடர்பு",
+      address: "முகவரி",
+      // framerHeader
+      searchPlace: "தயாரிப்புகளை இங்கே தேடுங்கள்...!",
+      // Farmerhome
+      sresults: "தேடியதற்கான விடைகள்",
+      myOrders: "என்னுடைய கட்டளைகள்",
+      // ProductDescription
+      cart: "பெட்டகத்தில் சேர்",
+      buy: "இப்போது வாங்க",
+      description: "விளக்கம்",
+      loginToAdd: "வண்டியில் உருப்படியைச் சேர்க்க உள்நுழைக.",
+      itemAdded: "உருப்படி வெற்றிகரமாக வண்டியில் சேர்க்கப்பட்டது.",
+      cannotAdd: "வண்டியில் உருப்படியைச் சேர்க்க முடியாது",
+      itemAlready: "பொருள் ஏற்கனவே வண்டியில் உள்ளது.",
+      // ProductCard
+      kg: "கிலோ",
+      // Farmer Register Page
+      phReq: "Phone number is required.",
+      paReq: "கடவுச்சொல் தேவை.",
+      naReq: "பெயர் தேவை",
+      adReq: "முகவரி தேவை",
+      fullName: "முழு பெயர்",
+      enterFName: "உங்கள் முழு பெயரை உள்ளிடவும்",
+      addr: "முகவரி",
+      enterAddr: "உங்கள் முகவரியை உள்ளிடவும்",
+      register: "பதிவு",
+      already: "ஏற்கனவே ஒரு கணக்கு உள்ளதா? உள்நுழைக",
+      // Farmer Signin
+      farmer: "உழவர்",
+      phone: "தொலைபேசி எண்",
+      pass: "கடவுச்சொல்",
+      enterPhone: "உங்கள் தொலைபேசி எண்ணை உள்ளிடவும்",
+      enterPass: "உங்கள் கடவுச்சொல்லை உள்ளிடவும்",
+      login: "உள்நுழைய",
+      signUp: "பதிவுபெறுக",
+      forgotPass: "கடவுச்சொல்லை மறந்துவிட்டீர்களா?",
+      // My Orders
+      orderStatus : "ஆர்டர் நிலை",
+      dispatched: "அனுப்பப்பட்டது",
+      trackOrder : "ஆர்டர்களைக் கண்காணிக்கவும்",
+      close : "நெருக்கமான",
+      currentOrders: "நடப்பு ஆர்டர்கள்",
+      completedOrders : "பூர்த்தி செய்யப்பட்ட ஆர்டர்கள்",
+      pName : "பொருளின் பெயர்",
+      tPrice : "மொத்த விலை",
+    }
+  };
+  
