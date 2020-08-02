45e056ea08f22a3b3054ce5b91e9876a5492d830
Gowtham C <gowthamdhss@gmail.com>
2020-08-02 15:11:18 +0530

    Telugu Language Support Added

---
+++ client/src/Localization/lang/telugu.js
@@ -0,0 +1,68 @@
+export default {
+    translation: {
+      greeting: "Hello",
+      // CartScreen
+      checkout: "చెక్అవుట్",
+      removeFromCart: "బండి నుండి తీసివేయండి",
+      goShop: "కొన్ని షాపింగ్ చేయండి.",
+      cantLoad: "ప్రస్తుతానికి బండిని లోడ్ చేయలేరు.",
+      cannotRemove: "అంశాన్ని తీసివేయలేరు.",
+      itemRemoved: "కార్ట్ నుండి అంశం తీసివేయబడింది.",
+      emptyCart: "బండి ఖాళీగా ఉంది.",
+      logout: "లాగౌట్",
+      // Checkout
+      orderPlaced: "ఆర్డర్ ఉంచబడింది.",
+      cannotOrder: "ఆర్డర్ ఉంచలేరు.",
+      summary: "ఉత్పత్తి సారాంశం",
+      gTotal: "సంపూర్ణ మొత్తము",
+      placeOrder: "ప్లేస్ ఆర్డర్",
+      customer: "వినియోగదారుని వివరాలు",
+      contact: "సంప్రదించండి",
+      address: "చిరునామా",
+      // framerHeader
+      searchPlace: "ఉత్పత్తులను ఇక్కడ శోధించండి...!",
+      // Farmerhome
+      sresults: "కోసం శోధన ఫలితాలు",
+      myOrders: "నా ఆదేశాలు",
+      // ProductDescription
+      cart: "బండికి జోడించండి",
+      buy: "ఇప్పుడే కొనండి",
+      description: "వివరణ",
+      loginToAdd: "కార్ట్ వివరణకు అంశాన్ని జోడించడానికి లాగిన్ అవ్వండి.",
+      itemAdded: "అంశం కార్ట్‌కు విజయవంతంగా జోడించబడింది.",
+      cannotAdd: "కార్ట్‌కు అంశాన్ని జోడించలేరు",
+      itemAlready: "అంశం ఇప్పటికే బండిలో ఉంది.",
+      // ProductCard
+      kg: "కిలొగ్రామ్",
+      // Farmer Register Page
+      phReq: "ఫోన్ నంబర్ అవసరం.",
+      paReq: "పాస్వర్డ్ అవసరం.",
+      naReq: "పేరు అవసరం",
+      adReq: "చిరునామా అవసరం",
+      fullName: "పూర్తి పేరు",
+      enterFName: "మీ పూర్తి పేరును నమోదు చేయండి",
+      addr: "చిరునామా",
+      enterAddr: "మీ చిరునామాను నమోదు చేయండి",
+      register: "నమోదు",
+      already: "ఖాతా కలిగి ఉన్నారా? సైన్ ఇన్ చేయండి",
+      // Farmer Signin
+      farmer: "రైతు",
+      phone: "ఫోను నంబరు",
+      pass: "పాస్వర్డ్",
+      enterPhone: "మీ ఫోన్ నెంబర్ ను ఎంటర్ చేయండి",
+      enterPass: "మీ పాస్వర్డ్ ని నమోదుచేయండి",
+      login: "ప్రవేశించండి",
+      signUp: "చేరడం",
+      forgotPass: "పాస్వర్డ్ మర్చిపోయారా?",
+      // My Orders
+      orderStatus : "ఆర్డర్ స్థితి",
+      dispatched: "పంపించబడ్డాడు",
+      trackOrder : "ట్రాక్ ఆర్డర్",
+      close : "దగ్గరగా",
+      currentOrders: "ప్రస్తుత ఆర్డర్లు",
+      completedOrders : "ఆర్డర్లు పూర్తయ్యాయి",
+      pName : "ఉత్పత్తి నామం",
+      tPrice : "మొత్తం విలువ",
+    }
+  };
+  
