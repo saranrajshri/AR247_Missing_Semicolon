7a2a7b3102f84bd0edfc4d8aa206417d2a67863d
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-08-02 10:43:50 +0530

    Adds order receipt screen to route

--- client/src/Screens/Farmer/Dashboard/Tabs/index.js
+++ client/src/Screens/Farmer/Dashboard/Tabs/index.js
@@ -3,5 +3,13 @@ import ProductDescription from "./ProductDescription/ProductDescription";
 import Checkout from "./Checkout/Checkout";
 import CartScreen from "./CartScreen/CartScreen";
 import MyOrders from "./MyOrders/MyOrders";
+import OrderReceipt from "./OrderReceipt/OrderReceipt";
 
-export { ProductsView, ProductDescription, Checkout, CartScreen, MyOrders };
+export {
+  ProductsView,
+  ProductDescription,
+  Checkout,
+  CartScreen,
+  MyOrders,
+  OrderReceipt
+};
