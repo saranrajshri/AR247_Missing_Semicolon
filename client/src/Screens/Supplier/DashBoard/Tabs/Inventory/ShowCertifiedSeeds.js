b800fa817820724d2534c0686ddfb3e5b95d6cd7
Dhanujha R <rampradha2016@gmail.com>
2020-08-02 14:57:58 +0530

    Adds UI for showing inventory

--- client/src/Screens/Supplier/DashBoard/Tabs/Inventory/ShowCertifiedSeeds.js
+++ client/src/Screens/Supplier/DashBoard/Tabs/Inventory/ShowCertifiedSeeds.js
@@ -1,6 +1,42 @@
 import React from "react";
+import DriversTable from "../ManageDrivers/components/DriversTable";
+import { Search, Button } from "semantic-ui-react";
+import CertifiedSeedsTable from "./CertifiedSeedsTable";
 
 const ShowCertifiedSeeds = () => {
-  return <div></div>;
+  return (
+    <div className="p-5">
+      <p className="title text-dark">Show Certified Seeds</p>
+      <div className="table-wrapper">
+        
+          <CertifiedSeedsTable
+            certifiedSeeds={drivers}
+            handleDelete={(seedsID) => {
+              // handleDeleteDriver(seedsID);
+              alert(seedsID);
+            }}
+          />
+      </div>
+    </div>
+  );
 };
 export default ShowCertifiedSeeds;
+
+const drivers = [{
+  _id: 6576585858858,
+  seedsID : 'SEED-1',
+  producerName : 'Raj',
+  contact : '1234567890',
+  address :  '76, car park',
+  expiryDate : '23-2-2001',
+  quantity : '25kg'
+},
+{
+  seedsID : 'SEED-2',
+  producerName : 'Raj',
+  contact : '1234567890',
+  address :  '76, car park',
+  expiryDate : '23-2-2001',
+  quantity : '25kg',
+  _id: 65765858588788,
+}]
